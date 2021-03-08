876cb2d05dae1b11b99584115901c340b3212986
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-03-09 03:02:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+bot_token = "1565813034:AAHsxXe45i5e-ygorfsK-WG14T07tKEFWAY"
 api_id = 2552682
 api_hash = "1d2eae5cf5dbbb0bfe65c7c78bb363c9"
 radio_link = "http://peridot.streamguys.com:7150/Mirchi"
